c915aa24ad6c76a2b144160ee34898bcaf3020e1
Michele Tasca <mic.tasca@gmail.com>
2021-05-16 11:02:21 +0200

    - move instance from var to func - add a whole bunch of contexts

--- Basic_HM_original.jl
+++ Basic_HM_original.jl
@@ -48,28 +48,31 @@ newvar() = (global TOPFREE = TOPFREE + 1; TOPFREE)
 
 mutable struct TypeVariable<:Type_
     id::Index
-    instance::Union{Nothing, Type_}
+    # instance::Union{Nothing, Type_}
     name::String
 end
 
-TypeVariable() = (v = newvar(); TypeVariable(v, nothing, string(v)))
+TypeVariable() = (v = newvar(); TypeVariable(v, string(v)))
+# TypeVariable() = (v = newvar(); TypeVariable(v, nothing, string(v)))
 
 struct TypeOperator<:Type_
     name::String
     types::Array{Type_}
 end
 struct Function<:Type_
+    instances::Dict{String, Type_}
     from_type::Type_ 
     to_type::Type_
 end
 # Function(from_type, to_type)::TypeOperator = TypeOperator("->", [from_type, to_type])
+Function(from_type, to_type)::Function = Function(Dict{Index, Type_}(), from_type, to_type)
 
 Integer_() = TypeOperator("int", [])  # Basic integer
 Bool_() = TypeOperator("bool", [])  # Basic bool
 
-pr(t::TypeVariable)::String = t.instance===nothing ? "T"*t.name : "T"*t.name*":"*(t.instance |> pr)
+pr(t::TypeVariable)::String = "T"*t.name # t.instance===nothing ? "T"*t.name : "T"*t.name*":"*(t.instance |> pr)
 pr(t::TypeOperator)::String = isempty(t.types) ? t.name : "($(join(t.types .|> pr, t.name)))"
-pr(t::Function)::String = "($(t.from_type |> pr)->$(t.to_type |> pr))"
+pr(t::Function)::String = "($(t.from_type |> pr)->$(t.to_type |> pr))" * (t.instances |> length == 0 ? "" : " where "*join(["T"*k*":"*pr(v) for (k,v) in t.instances], ", "))
 
 # =======================================================#
 # Type inference machinery
@@ -95,7 +98,7 @@ pr(t::Function)::String = "($(t.from_type |> pr)->$(t.to_type |> pr))"
 # """
 
 
-function analyse(node::Identifier, env, non_generic::Array{Type_}=Array{Type_}([]))
+function analyse(node::Identifier, env::Dict{String, Type_}, non_generic::Array{Type_}=Array{Type_}([]))
     return get_type(node.name, env, non_generic)
 end
 function analyse(node::Apply, env, non_generic::Array{Type_}=Array{Type_}([]))
@@ -103,11 +106,11 @@ function analyse(node::Apply, env, non_generic::Array{Type_}=Array{Type_}([]))
     # println("non_generic: ", non_generic .|> pr)
     fun_type = analyse(node.fn, env, non_generic)
     arg_type = analyse(node.arg, env, non_generic)
-    result_type = TypeVariable()
-    unify(prune(Function(arg_type, result_type)), prune(fun_type))
+    result_type = TypeVariable() ################################ EXTREMELY IMPORTANT THING that is happening!!! (it's what's broken in the Other One)
+    unify(prune(Function(arg_type, result_type), env), prune(fun_type, env), env)
     return result_type
 end
-function analyse(node::Lambda, env, non_generic::Array{Type_}=Array{Type_}([]))
+function analyse(node::Lambda, env::Dict{String, Type_}, non_generic::Array{Type_}=Array{Type_}([]))
     # println("env: ", [k*":"*pr(v) for (k, v) in env])
     # println("non_generic: ", non_generic .|> pr)
     arg_type = TypeVariable()
@@ -116,9 +119,9 @@ function analyse(node::Lambda, env, non_generic::Array{Type_}=Array{Type_}([]))
     new_non_generic = copy(non_generic)
     push!(new_non_generic, arg_type)
     result_type = analyse(node.body, new_env, new_non_generic)
-    return Function(arg_type, result_type)
+    return Function(new_env, arg_type, result_type)
 end
-function analyse(node::Let, env, non_generic::Array{Type_}=Array{Type_}([]))
+function analyse(node::Let, env::Dict{String, Type_}, non_generic::Array{Type_}=Array{Type_}([])) #dc
     # println("env: ", [k*":"*pr(v) for (k, v) in env])
     # println("non_generic: ", non_generic .|> pr)
     defn_type = analyse(node.defn, env, non_generic)
@@ -126,7 +129,7 @@ function analyse(node::Let, env, non_generic::Array{Type_}=Array{Type_}([]))
     new_env[node.v] = defn_type
     return analyse(node.body, new_env, non_generic)
 end
-function analyse(node::Letrec, env, non_generic::Array{Type_}=Array{Type_}([]))
+function analyse(node::Letrec, env::Dict{String, Type_}, non_generic::Array{Type_}=Array{Type_}([])) #dc
     # println("env: ", [k*":"*pr(v) for (k, v) in env])
     # println("non_generic: ", non_generic .|> pr)
     new_type = TypeVariable()
@@ -135,7 +138,7 @@ function analyse(node::Letrec, env, non_generic::Array{Type_}=Array{Type_}([]))
     new_non_generic = copy(non_generic)
     push!(new_non_generic, new_type)
     defn_type = analyse(node.defn, new_env, new_non_generic)
-    unify(prune(new_type), prune(defn_type))
+    unify(prune(new_type, env), prune(defn_type, env), env)
     return analyse(node.body, new_env, non_generic)
 end
 
@@ -145,7 +148,7 @@ end
 function get_type(name, env, non_generic)
     # """Get the type of identifier name from the type environment env.
     if name in keys(env)
-        return fresh(env[name], non_generic)
+        return fresh(env[name], non_generic, env)
     elseif is_integer_literal(name)
         return Integer_()
     else
@@ -155,29 +158,29 @@ end
 
 
 
-function fresh(t, non_generic)
+function fresh(t, non_generic, ctxout::Dict{String, Type_})
     # Makes a copy of a type expression.
     # The type t is copied. The the generic variables are duplicated and the
     # non_generic variables are shared.
     mappings = Dict{TypeVariable, TypeVariable}()  # A mapping of TypeVariables to TypeVariables
     
-    function freshrec(tp)
-        p = prune(tp)
+    function freshrec(tp, ctx::Dict{String, Type_})
+        p = prune(tp, ctx)
         if p isa TypeVariable
-            if is_generic(p, non_generic)
+            if is_generic(p, non_generic, ctx)
                 get!(mappings, p, TypeVariable()) 
                 #The the generic variables are duplicated, the old->new map is stored in Mappings so that same olds have same news.  Mappings is != from env !!!!!!
             else
                 p
             end
         elseif p isa TypeOperator
-            return TypeOperator(p.name, [freshrec(x) for x in p.types])
+            return TypeOperator(p.name, [freshrec(x, ctx) for x in p.types])
         elseif p isa Function
-            return Function(freshrec(p.from_type), freshrec(p.to_type))
+            return Function(freshrec(p.from_type, ctx), freshrec(p.to_type, ctx))
         end
     end
     
-    freshrec(t)
+    freshrec(t, ctxout)
 end
 
 
@@ -185,40 +188,40 @@ end
 # Makes the types t1 and t2 the same.
 # a = prune(t1)
 # b = prune(t2)
-function unify(a::TypeVariable, b::Type_,)
+function unify(a::TypeVariable, b::Type_, ctx::Dict{String, Type_})
     if a != b
-        if occurs_in_type(a, b)
+        if occurs_in_type(a, b, ctx)
             throw(InferenceError("recursive unification"))
         end
-        a.instance = b
+        ctx[a.name] = b
     end
 end
-function unify(a::TypeVariable, b::TypeVariable) # SAME as above
+function unify(a::TypeVariable, b::TypeVariable, ctx::Dict{String, Type_}) # SAME as above
     if a != b
-        if occurs_in_type(a, b)
+        if occurs_in_type(a, b, ctx)
             throw(InferenceError("recursive unification"))
         end
-        a.instance = b
+        ctx[a.name] = b
     end
 end
-function unify(a::Type_, b::TypeVariable) 
-    unify(prune(b), prune(a))
+function unify(a::Type_, b::TypeVariable, ctx::Dict{String, Type_}) 
+    unify(b, a, ctx)
 end
-function unify(a::TypeOperator, b::TypeOperator) 
+function unify(a::TypeOperator, b::TypeOperator, ctx::Dict{String, Type_}) 
     if (a.name != b.name) || (length(a.types) != length(b.types))
         throw(InferenceError("Type mismatch: $(a) != $(b)"))
     end
-    for (p, q) in zip(a.types, b.types)
-        unify(prune(p), prune(q))
+    for (p, q) in zip(a.types, b.types, ctx::Dict{String, Type_})
+        unify(prune(p, ctx), prune(q, ctx), ctx)
     end
 end
-function unify(a::Function, b::Function) 
+function unify(a::Function, b::Function, ctx::Dict{String, Type_}) 
     for (p, q) in zip([a.from_type, a.to_type], [b.from_type, b.to_type])
-        unify(prune(p), prune(q))
+        unify(prune(p, a.instances), prune(q, b.instances), ctx) ################ THIS is there i think it should have a SECOND (TYPE) LEVEL APPLY.......
     end
 end
-function unify(a, b) 
-    throw(DomainError("There are other cases?? ($(a), $(b))"))
+function unify(a, b, ctx::Dict{String, Type_}) 
+    throw(DomainError("There are other cases?? ($(a)  <- and -> $(b))"))
 end
 
 # """Returns the currently defining instance of t.
@@ -228,13 +231,21 @@ end
 # a type operator; i.e. it will skip instantiated variables, and will
 # actually prune them from expressions to remove long chains of instantiated
 # variables.
-function prune(t::Type_)
-    if ((t isa TypeVariable) && (t.instance !== nothing))
-        t.instance = prune(t.instance)
-        return t.instance
+
+# Here, pruning is NOT pruning tho: this is because you DONT have the FULL context stack :(
+function prune(t::Type_, context::Dict{String, Type_})
+    if (t isa TypeVariable) get(context, t.name, t)
+    else t
     end
-    return t
 end
+        
+# function prune(t::Type_)
+#     if ((t isa TypeVariable) && (t.instance !== nothing))
+#         t.instance = prune(t.instance)
+#         return t.instance
+#     end
+#     return t
+# end
 
 # """Checks whether a given variable occurs in a list of non-generic variables.
 # This means: that a given type-VARIABLE v either IS, or IS USED BY (is THE INSTANCE OF A VARIABLE IN) type type2 in non_generic ...
@@ -243,26 +254,26 @@ end
 # [[recursively in nested contexts]]
 # Note: Must be called with v pre-pruned
 
-function is_generic(v, non_generic)
-    return ! occurs_in(v, non_generic)
+function is_generic(v, non_generic, ctx::Dict{String, Type_})
+    return ! occurs_in(v, non_generic, ctx)
 end
 
-function occurs_in_type(v, type2)
+function occurs_in_type(v, type2, ctx::Dict{String, Type_})
     # """Checks whether a type variable occurs in a type expression.
     # Note: Must be called with v pre-pruned
-    pruned_type2 = prune(type2)
+    pruned_type2 = prune(type2, ctx)
     if pruned_type2 == v
         return true
     elseif pruned_type2 isa TypeOperator
-        return occurs_in(v, pruned_type2.types)
+        return occurs_in(v, pruned_type2.types, ctx)
     elseif pruned_type2 isa Function
-        return occurs_in(v, [pruned_type2.from_type, pruned_type2.to_type])
+        return occurs_in(v, [pruned_type2.from_type, pruned_type2.to_type], pruned_type2.instances)
     end
     return false
 end
 
-function occurs_in(t, types)
-    return [occurs_in_type(t, t2) for t2 in types] |> any
+function occurs_in(t, types, ctx::Dict{String, Type_})
+    return [occurs_in_type(t, t2, ctx) for t2 in types] |> any
 end
 
 function is_integer_literal(name)
@@ -296,6 +307,7 @@ my_env = Dict{String, Type_}("pair"=> Function(var1, Function(var2, pair_type)),
             "zero"=> Function(Integer_(), Bool_()),
             "pred"=> Function(Integer_(), Integer_()),
             "times"=> Function(Integer_(), Function(Integer_(), Integer_())))
+my_env = Dict{String, Type_}("pair"=> Function(var1, Function(var2, pair_type)))
 
 pair = Apply(Apply(Identifier("pair"),
                     Apply(Identifier("f"),
